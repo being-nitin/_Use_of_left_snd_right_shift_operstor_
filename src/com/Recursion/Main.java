52ce5b256840e05415191e03ff6aa1c506048cc8
Nitin <nsinghboss02@gmail.com>
2022-01-18 10:54:50 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,17 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Java program to use left and right shift operator:
+	 */
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a<<b);
+        System.out.println(a>>b);
+
+    }
+}
